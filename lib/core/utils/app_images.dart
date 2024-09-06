7bfcc4b0de64c107b7270f4cefdc388e2ab5f623
bahnas <bahnas114@gmail>
2024-09-06 19:37:17 +0300

    add images done

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar-1.svg
+  static const String imagesAvatar1 = "assets/images/avatar-1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar-2.svg
+  static const String imagesAvatar2 = "assets/images/avatar-2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCardBack
+  /// assets/images/card-back.svg
+  static const String imagesCardBack = "assets/images/card-back.svg";
+
+  /// Assets for imagesConvertCard
+  /// assets/images/convert-card.svg
+  static const String imagesConvertCard = "assets/images/convert-card.svg";
+
+  /// Assets for imagesDashBoard
+  /// assets/images/dashBoard.svg
+  static const String imagesDashBoard = "assets/images/dashBoard.svg";
+
+  /// Assets for imagesExpneses
+  /// assets/images/expneses.svg
+  static const String imagesExpneses = "assets/images/expneses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesInvestment
+  /// assets/images/investment.svg
+  static const String imagesInvestment = "assets/images/investment.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assets/images/wallet-account.svg
+  static const String imagesWalletAccount = "assets/images/wallet-account.svg";
+}
+
